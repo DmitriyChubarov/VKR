7fd1ee1631c79d0a3462f55cb9f2f0b5d061f6af
Dmitriy <cubarrv@gmail.com>
2024-10-12 18:28:37 +0300

    correlation + resize 1.1

--- correlation.py
+++ correlation.py
@@ -1,17 +1,18 @@
 from PIL import Image
 from urllib.request import urlopen
 import numpy as np
-# width, height = img_2.size
-    # a = img_2.crop((-2, -2, width + 2, height + 2))
 
 img_1 = Image.open(urlopen('https://i.ibb.co/6ZK2BZj/pc-1.jpg'))
 img_1 = img_1.convert('L')
 img_2 = Image.open(urlopen('https://i.ibb.co/WyWPPNt/pc-2.jpg'))
 img_2 = img_2.convert('L')
+img_3 = Image.open(urlopen('https://i.ibb.co/8dp99kR/200-200-1.jpg'))
+img_3 = img_3.convert('L')
 
 def size(x,y):
     w1, h1 = x.size
     w2, h2 = y.size
+
     if w1 * h1 == w2 * h2:
         return x, y
     elif w1 * h1 > w2 * h2:
@@ -20,13 +21,12 @@ def size(x,y):
             c = a // 2 + 1
         else:
             c = a / 2
-
         if b % 2 != 0:
             d = b // 2 + 1
         else:
             d = b / 2
 
-        y = y.crop((- (a // 2), - (b // 2), w1 + c, h1 + d))
+        y = y.crop((- (a // 2), - (b // 2), w2 + c, h2 + d))
     else:
         a, b = w2 - w1, h2 - h1
         if a % 2 != 0:
@@ -42,7 +42,6 @@ def size(x,y):
         x = x.crop((- (a // 2), - (b // 2), w1 + c, h1 + d))
 
     return x, y
-
 def cor(x, y):
 
     ux = np.mean(x)
@@ -53,10 +52,9 @@ def cor(x, y):
 
     return ((numerator / denominator) + 1) / 2
 
-img_1_new, img_2_new = size(img_1, img_2)
-print(cor(img_1_new, img_2_new))
-img_1_new.show()
-img_2_new.show()
+img_1_new, img_3_new = size(img_1, img_3)
+print(cor(img_1_new, img_3_new))
+
 
 
 
